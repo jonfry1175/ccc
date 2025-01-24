429666dc73529ecb0a6cbb91123c85dacd4920d8
Galuh <galuhpradipta95@gmail.com>
2025-01-24 21:09:29 +0700

    add gtm on head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import Footer from "@/components/layout/footer";
 import Navbar from "@/components/layout/navbar";
+import { GoogleTagManager } from "@next/third-parties/google";
 import type { Metadata } from "next";
 import { Geist, Poppins } from "next/font/google";
-import Script from 'next/script';
+import Head from "next/head";
 import "./globals.css";
+
 
 const geist = Geist({
   variable: "--font-geist",
@@ -30,21 +32,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="id" className="scroll-smooth">
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','AW-11565537272');`
-        }}
-      />
+      <Head>
+        <GoogleTagManager gtmId="AW-11565537272" />
+      </Head>
       <body className={`${geist.variable} ${poppins.variable} antialiased`}>
-        <noscript>
-          <iframe src="https://www.googletagmanager.com/ns.html?id=AW-11565537272" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }} />
-        </noscript>
         <Navbar />
         {children}
         <Footer />
