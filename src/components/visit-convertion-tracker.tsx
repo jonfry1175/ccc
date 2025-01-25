844c288464ad1282aaac7de1a24cda29ba0114c9
Galuh <galuhpradipta95@gmail.com>
2025-01-25 15:26:44 +0700

    fix how to send event of sendGTMEvent

--- src/components/visit-convertion-tracker.tsx
+++ src/components/visit-convertion-tracker.tsx
@@ -5,10 +5,8 @@ import { useEffect } from 'react'
 
 export default function VisitConversionTracker(): React.ReactNode {    
     useEffect(() => {
-        sendGTMEvent({
-            event: 'conversion',
-            'send_to': 'AW-11565537272/W0gdCLmEkIwaEPiv8Ior'
-        })
+        console.log('triggered')
+        sendGTMEvent({ event: 'conversion', value: { 'send_to': 'AW-11565537272/W0gdCLmEkIwaEPiv8Ior' } }) 
     }, [])
 
 
